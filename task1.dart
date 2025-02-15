3f0d122f49133a35e86bb035c0759c44419328de
Tarbian <oleh.vitruk.20@pnu.edu.ua>
2025-02-15 14:36:40 +0200

    Update task1.dart

--- task1.dart
+++ task1.dart
@@ -1,37 +1,47 @@
 import 'dart:io';
 import 'dart:math';
 
-void main() {
-  const maxAttempts = 4;
-  final posisiton = Random().nextInt(10) + 1;
-  int attempt = 0;
-  String result = "Спроби вичерпано!\nТи програв :(";
+const maxAttempts = 4;
 
+void main() {
+  final position = Random().nextInt(10) + 1;
+  int attempt = 0;
+  bool hasWon = false;
+  
   print("Вгадай де російський корабель!\nВведи число від 1 до 10:");
 
-  while (attempt < maxAttempts) {
-    String? input = stdin.readLineSync();
-    if (input == '' || input == null) {
-      print("А де цифри?");
+  while (attempt < maxAttempts && !hasWon) {
+    int guess = getValidIntInput(
+      "Введіть правильне число від 1 до 10:",
+      List.generate(10, (i) => i + 1)
+    );
+    
+    if (guess < position) {
+      print("Занадто низько! Спробуй знову.");
+    } else if (guess > position) {
+      print("Занадто високо! Спробуй знову.");
     } else {
-      try {
-        int? quess = int.parse(input);
-        if (quess < 1 || quess > 10) {
-          print("Число має бути у діапазоні 1 - 10");
-        } else if (quess < posisiton) {
-          print("Занадто низько! Спробуй знову.");
-          attempt++;
-        } else if (quess > posisiton) {
-          print("Занадто високо! Спробуй знову.");
-          attempt++;
-        } else if (quess == posisiton) {
-          result = "Вітаю! Ти вгадав, корабель на позиції $posisiton!";
-          break;
-        }
-      } on FormatException {
-        print("Програма приймає тільки цілі числа!\nСпробуй ще раз:");
-      }
+      hasWon = true;
     }
+    attempt++;
   }
-  print(result);
+  
+  print(hasWon 
+    ? "Вітаю! Ти вгадав, корабель на позиції $position!" 
+    : "Спроби вичерпано!\nТи програв :(");
+}
+
+int getValidIntInput(String errorMessage, [List<int>? allowedValues]) {
+  while (true) {
+    try {
+      String? input = stdin.readLineSync();
+      if (input != null && input.isNotEmpty) {
+        int number = int.parse(input);
+        if (allowedValues == null || allowedValues.contains(number)) {
+          return number;
+        }
+      }
+    } catch (e) {}
+    print(errorMessage);
+  }
 }
